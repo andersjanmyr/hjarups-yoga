f35338a7bc346415109f5239b8820d337a4191e6
Anders Janmyr <anders@janmyr.com>
2011-05-22 19:13:19 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Equilo::Application.config.secret_token = '19ad34493fd36cbd60f91d6c6b7c858fd5cf99dd0798678e8252c06455830eaeb450fe5a1f971faf75bf0e2dd46dfddd6e3d6d5b7034c39ea8fc676752abcaab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Equilo::Application.config.session_store :cookie_store, :key => '_equilo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Equilo::Application.config.session_store :active_record_store
