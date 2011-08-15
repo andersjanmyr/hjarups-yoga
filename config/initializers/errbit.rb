d358b53b9516e5b6d3d1dfc6640d8c84cfc8a6b3
Anders Janmyr <anders@janmyr.com>
2011-08-15 16:59:25 +0200

    configured errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,5 @@
 HoptoadNotifier.configure do |config|
-  config.api_key = '44df306c9dfcf7471280f8f87c520bfe'
+  config.api_key = 'bd999b27c9309b9cf1437f4b29e3c15c'
   config.host    = 'errbit-anders.heroku.com'
   config.port    = 80
   config.secure  = config.port == 443
